696e8d8c13b69311d7ad4abf7ad32c8ead63d14c
Hug0H3nrique <163132634+Hug0H3nrique@users.noreply.github.com>
2024-05-21 11:03:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,380 @@
+print("Bem-vindo ao sistema de consulta da tabela periódica!")
+funciona = True
+while funciona == True:
+  numeroTabela = int(input("Digite o numero atomico do elemento (Digite 0 para encerrar a consulta): "))
+
+  if numeroTabela == 1:
+    print("Elemento: Hidrogênio")
+    print("Símbolo: H")
+    print("Valor da massa: 1,008")
+
+  elif numeroTabela == 2:
+    print("Elemento: Hélio")
+    print("Símbolo: He")
+    print("Valor da massa: 4,003")
+
+  if numeroTabela == 3:
+    print("Elemento: Lítio")
+    print("Simbolo: Li")
+    print("Valor da massa: 6,941")
+
+  elif numeroTabela == 4:
+    print("Elemento: Berílio")
+    print("Símbolo: Be")
+    print("Valor da massa: 9,012")
+  if numeroTabela == 5:
+    print("Elemento: Boro")
+    print("Símbolo: B")
+    print("Valor da massa: 10,811")
+
+  elif numeroTabela == 6:
+    print("Elemento: Carbono")
+    print("Símbolo: C")
+    print("Valor da massa: 12,011")
+  if numeroTabela == 7:
+    print("Elemento: Nitrogênio")
+    print("Símbolo: N")
+    print("Valor da massa: 14,007")
+  elif numeroTabela == 8:
+    print("Elemento: Oxigênio")
+    print("Símbolo: O")
+    print("Valor da massa: 15,999")
+  if numeroTabela== 9:
+    print("Elemento: Flúor")
+    print("Símbolo: F")
+    print("Valor da massa: 18,998")
+  elif numeroTabela == 10:
+    print("Elemento: Neônio")
+    print("Símbolo: Ne")
+    print("Valor da massa: 20,18")
+  if numeroTabela == 11:
+    print("Elemento: Sódio")
+    print("Símbolo: Na" )
+    print("Valor da massa: 22,99")
+  elif numeroTabela == 12:
+    print("Elemento: Magnésio")
+    print("Símbolo: Mg")
+    print("Valor da massa: 24,305")
+  if numeroTabela == 13:
+    print("Elemento: Alumínio")
+    print("Simbolo: Al")
+    print("Valor da massa: 26,982")
+  elif numeroTabela == 14:
+    print("Elemento: Silício")
+    print("Símbolo: Si")
+    print("Valor da massa: 28,086")
+  if numeroTabela == 15:
+    print("Elemento: Fósforo")
+    print("Siímbolo: P")
+    print("Valor da massa: 30,974")
+  elif numeroTabela == 16:
+    print("Elemento: Enxofre")
+    print("Símbolo: S")
+    print("Valor da massa: 32,065")
+  if numeroTabela == 17:
+    print("Elemento: Cloro")
+    print("Símbolo: Cl")
+    print("Valor da massa: 35,453")
+  elif numeroTabela == 18:
+    print("Elemento: Argônio")
+    print("Símbolo: Ar")
+    print("Valor da massa: 39,948")
+  if numeroTabela == 19:
+    print("Elemento: Potássio")
+    print("Símbolo: K")
+    print("Valor da massa: 39,098")
+  elif numeroTabela == 20:
+    print("Elemento: Cálcio")
+    print("Siímbolo: Ca")
+    print("Valor da massa: 40,078")
+  if numeroTabela == 21:
+    print("Elemento: Escândio")
+    print("Símbolo: Sc")
+    print("Valor da massa: 44,956")
+  elif numeroTabela == 22:
+    print("Elemento: Titânio")
+    print("Símbolo: Ti")
+    print("Valor da massa: 47,867")
+  if numeroTabela == 23: 
+    print("Elemento: Vanádio")
+    print("Símbolo: V")
+    print("Valor da massa: 50,942")
+  elif numeroTabela == 24:
+    print("Elemento: Cromo")
+    print("Símbolo: Cr")
+    print("Valor da massa: 51,996")
+  if numeroTabela == 25:
+      print("Elemento: Manganês")
+      print("Símbolo: Mn")
+      print("Valor da massa: 54,938")
+  elif numeroTabela == 26:
+      print("Elemento: Ferro")
+      print("Símbolo: Fe")
+      print("Valor da massa: 55,845")
+  if numeroTabela == 27:
+      print("Elemento: Cobalto")
+      print("Símbolo: Co")
+      print("Valor da massa: 58,933")
+  elif numeroTabela == 28:
+      print("Elemento: Níquel")
+      print("Símbolo: Ni")
+      print("Valor da massa: 58,693")
+  if numeroTabela == 29:
+      print("Elemento: Cobre")
+      print("Símbolo: Cu")
+      print("Valor da massa: 63,546")
+  elif numeroTabela == 30:
+      print("Elemento: Zinco")
+      print("Símbolo: Zn")
+      print("Valor da massa: 65,38")
+  if numeroTabela == 31:
+      print("Elemento: Gálio")
+      print("Símbolo: Ga")
+      print("Valor da massa: 69,723")
+  elif numeroTabela == 32:
+      print("Elemento: Germânio")
+      print("Símbolo: Ge")
+      print("Valor da massa: 72,63")
+  if numeroTabela == 33:
+      print("Elemento: Arsênio")
+      print("Símbolo: As")
+      print("Valor da massa: 74,922")
+  elif numeroTabela == 34:
+      print("Elemento: Selênio")
+      print("Símbolo: Se")
+      print("Valor da massa: 78,971")
+  if numeroTabela == 35:
+      print("Elemento: Bromo")
+      print("Símbolo: Br")
+      print("Valor da massa: 79,904")
+  elif numeroTabela == 36:
+      print("Elemento: Cádmio")
+      print("Símbolo: Cd")
+      print("Valor da massa: 112,411")
+  if numeroTabela == 37:
+      print("Elemento: Índio")
+      print("Símbolo: In")
+      print("Valor da massa: 114,818")
+  elif numeroTabela == 38:
+      print("Elemento: Estanho")
+      print("Símbolo: Sn")
+      print("Valor da massa: 118,711")
+  if numeroTabela == 39:
+      print("Elemento: Antimônio")
+      print("Símbolo: Sb")
+      print("Valor da massa: 121,760")
+  elif numeroTabela == 40:
+      print("Elemento: Telúrio")
+      print("Símbolo: Te")
+      print("Valor da massa: 127,60")
+  if numeroTabela == 41:
+      print("Elemento: Iodo")
+      print("Símbolo: I")
+      print("Valor da massa: 126,904")
+  elif numeroTabela == 42:
+      print("Elemento: Xenônio")
+      print("Símbolo: Xe")
+      print("Valor da massa: 131,293")
+  if numeroTabela == 43:
+      print("Elemento: Césio")
+      print("Símbolo: Cs")
+      print("Valor da massa: 132,905")
+  elif numeroTabela == 44:
+      print("Elemento: Bário")
+      print("Símbolo: Ba")
+      print("Valor da massa: 137,327")
+  if numeroTabela == 45: 
+      print("Elemento: Lantânio")
+      print("Símbolo: La")
+      print("Valor da massa: 138,905")
+  elif numeroTabela == 46:
+      print("Elemento: Cério")
+      print("Símbolo: Ce")
+      print("Valor da massa: 140,116")
+  if numeroTabela == 47:
+      print("Elemento: Praseodímio")
+      print("Símbolo: Pr")
+      print("Valor da massa: 140,908")
+  elif numeroTabela == 48:
+      print("Elemento: Neodímio")
+      print("Símbolo: Nd")
+      print("Valor da massa: 144,242")
+  if numeroTabela == 49:
+      print("Elemento: Prata")
+      print("Símbolo: Pd")
+      print("Valor da massa: 145,966")
+  elif numeroTabela == 50:
+      print("Elemento: Ouro")
+      print("Símbolo: Au")
+      print("Valor da massa: 196,967")
+  if numeroTabela == 51:
+      print("Elemento: Mercúrio")
+      print("Símbolo: Hg")
+      print("Valor da massa: 200,59")
+  elif numeroTabela == 52:
+      print("Elemento: Tálio")
+      print("Símbolo: Tl")
+      print("Valor da massa: 204,38")
+  if numeroTabela == 53:
+      print("Elemento: Chumbo")
+      print("Símbolo: Pb")
+      print("Valor da massa: 207,2")
+  elif numeroTabela == 54:
+      print("Elemento: Bismuto")
+      print("Símbolo: Bi")
+      print("Valor da massa: 208,98")
+  if numeroTabela == 55:
+      print("Elemento: Polônio")
+      print("Símbolo: Po")
+      print("Valor da massa: 209")
+  elif numeroTabela == 56:
+      print("Elemento: Astato")
+      print("Símbolo: At")
+      print("Valor da massa: 210")
+  if numeroTabela == 57:
+      print("Elemento: Radônio")
+      print("Símbolo: Rn")
+      print("Valor da massa: 222")
+  elif numeroTabela == 58:
+      print("Elemento: Frâncio")
+      print("Símbolo: Fr")
+      print("Valor da massa: 223")
+  if numeroTabela == 59:
+      print("Elemento: Radium")
+      print("Símbolo: Ra")
+      print("Valor da massa: 226,025")
+  elif numeroTabela == 60:
+      print("Elemento: Actínio")
+      print("Símbolo: Ac")
+      print("Valor da massa: 227,028")
+  if numeroTabela == 61:
+      print("Elemento: Thorium")
+      print("Símbolo: Th")
+      print("Valor da massa: 232,038")
+  elif numeroTabela == 62:
+      print("Elemento: Protactínio")
+      print("Símbolo: Pa")
+      print("Valor da massa: 231,036")
+  if numeroTabela == 63:
+      print("Elemento: Urânio")
+      print("Símbolo: U")
+      print("Valor da massa: 238,029")
+  elif numeroTabela == 64:
+      print("Elemento: Netúnio")
+      print("Símbolo: Np")
+      print("Valor da massa: 237")
+  if numeroTabela == 65:
+      print("Elemento: Plutônio")
+      print("Símbolo: Pu")
+      print("Valor da massa: 244")
+  elif numeroTabela == 66:
+      print("Elemento: Amerício")
+      print("Símbolo: Am")
+      print("Valor da massa: 243")
+  if numeroTabela == 67:
+      print("Elemento: Cúrio")
+      print("Símbolo: Cm")
+      print("Valor da massa: 247")
+  elif numeroTabela == 68:
+      print("Elemento: Berquélio")
+      print("Símbolo: Bk")
+      print("Valor da massa: 247")
+  if numeroTabela == 69:
+      print("Elemento: Califórnio")
+      print("Símbolo: Cf")
+      print("Valor da massa: 251")
+  elif numeroTabela == 70:
+      print("Elemento: Einstênio")
+      print("Símbolo: Es")
+      print("Valor da massa: 252")
+  if numeroTabela == 71:
+      print("Elemento: Férmio")
+      print("Símbolo: Fm")
+      print("Valor da massa: 257")
+  elif numeroTabela == 72:
+      print("Elemento: Mendelevium")
+      print("Símbolo: Md")
+      print("Valor da massa: 258")
+  if numeroTabela == 73:
+      print("Elemento: Nobélio")
+      print("Símbolo: No")
+      print("Valor da massa: 259")
+  elif numeroTabela == 74:
+      print("Elemento: Láurênio")
+      print("Símbolo: Lr")
+      print("Valor da massa: 262")
+  if numeroTabela == 75:
+      print("Elemento: Rutherfórdio")
+      print("Símbolo: Rf")
+      print("Valor da massa: 261")
+  elif numeroTabela == 76:
+      print("Elemento: Dubnium")
+      print("Símbolo: Db")
+      print("Valor da massa: 262")
+  if numeroTabela == 77:
+      print("Elemento: Seabórgio")
+      print("Símbolo: Sg")
+      print("Valor da massa: 266")
+  elif numeroTabela == 78:
+      print("Elemento: Bhór")
+      print("Símbolo: Bh")
+      print("Valor da massa: 264")
+  if numeroTabela == 79:
+      print("Elemento: Hássio")
+      print("Símbolo: Hs")
+      print("Valor da massa: 269")
+  elif numeroTabela == 80:
+      print("Elemento: Meitnério")
+      print("Símbolo: Mt")
+      print("Valor da massa: 268")
+  if numeroTabela == 81:
+      print("Elemento: Darmstádio")
+      print("Símbolo: Ds")
+      print("Valor da massa: 281")
+  elif numeroTabela == 82:
+      print("Elemento: Roentgênio")
+      print("Símbolo: Rg")
+      print("Valor da massa: 281")
+  if numeroTabela == 83:
+      print("Elemento: Copernício")
+      print("Símbolo: Cn")
+      print("Valor da massa: 285")
+  elif numeroTabela == 84:
+      print("Elemento: Nihônio")
+      print("Símbolo: Nh")
+      print("Valor da massa: 286")
+  if numeroTabela == 85:
+      print("Elemento: Fleróvio")
+      print("Símbolo: Fl")
+      print("Valor da massa: 289")
+  elif numeroTabela == 86:
+      print("Elemento: Moscóvio")
+      print("Símbolo: Mc")
+      print("Valor da massa: 289")
+  if numeroTabela == 87:
+      print("Elemento: Livermório")
+      print("Símbolo: Lv")
+      print("Valor da massa: 293")
+  elif numeroTabela == 88:
+      print("Elemento: Tennessínio")
+      print("Símbolo: Ts")
+      print("Valor da massa: 294")
+  if numeroTabela == 89:
+      print("Elemento: Oganessônio")
+      print("Símbolo: Og")
+      print("Valor da massa: 294")
+  elif numeroTabela == 90:
+      print("Elemento: Ununtrio")
+      print("Símbolo: Uut")
+      print("Valor da massa: 294")
+  if numeroTabela == 91:
+      print("Elemento: Ununbíquio")
+      print("Símbolo: Uub")
+      print("Valor da massa: 294")
+  elif numeroTabela == 92:
+      print("Elemento: Ununquadínio")
+      print("Símbolo: Uuq")
+      print("Valor da massa: 294")
+  if numeroTabela == 93:
+      
+  
